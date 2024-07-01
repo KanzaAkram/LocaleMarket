dda285b6fafaa3b25bf395909e92d54d9a11554c
KANZA MUHAMMAD AKRAM <85638781+KanzaAkram@users.noreply.github.com>
2024-07-01 15:24:48 +0000

    removed best seller section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Banner from "../components/Banner";
 import { Feature } from "../components/Feature";
 import { Review } from "../components/Review";
-import { BestSeller } from "../components/BestSeller";
 import { NewDesignCover } from "../components/NewDesignCover";
 import { Blog } from "../components/Blog";
 import Sidebar from "../components/Sidebar";
@@ -13,7 +12,6 @@ const Home = () => {
       <Banner />
       <Feature />
       <Review />
-      <BestSeller />
       <NewDesignCover />
       <Blog />
       <Sidebar />
